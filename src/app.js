f965aa8447c55351e8eb3e60508e0a882c43c133
t-vo90 <thuan@hotmail.de>
2021-06-03 15:16:47 +0200

    Restructure folders

--- src/app.js
+++ src/app.js
@@ -7,8 +7,8 @@ var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 
-var indexRouter = require('./routes/index')
-var usersRouter = require('./routes/users')
+var indexRouter = require('./express/routes/index')
+var usersRouter = require('./express/routes/users')
 
 var app = express()
 
