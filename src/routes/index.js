893425ab15992db1f0b6d92a7f7463635987cd54
t-vo90 <thuan@hotmail.de>
2021-06-08 22:56:46 +0200

    Add new Require

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,11 @@ const express = require('express')
 
 const router = express.Router()
 
+const User = require('../models/db')
+
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Whatever I Want you to be' })
+  res.render('index', { title: 'NoCrastinate' })
 })
 
 module.exports = router
