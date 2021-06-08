6cd6f089e3b3718d8c5c3ed32651208dff98e787
t-vo90 <thuan@hotmail.de>
2021-06-08 22:55:26 +0200

    Remove example code

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,25 +4,7 @@ const { request } = require('../app')
 
 const router = express.Router()
 
-const User = require('../models/user')
-const Photo = require('../models/photo')
-
-const mihri = new User('mihri', 35)
-const armagan = new User('armagan', 36)
-
-const steve = new User('steve', 21)
-steve.bio = 'An awesome hacker who has seen it all, and now sharing them all with you.'
-
-const berlinPhoto = new Photo('berlin.jpg')
-const munichPhoto = new Photo('munich.jpg')
-
-steve.addPhoto(berlinPhoto)
-steve.addPhoto(munichPhoto)
-
-armagan.likePhoto(berlinPhoto)
-mihri.likePhoto(berlinPhoto)
-
-const users = [mihri, armagan, steve]
+const User = require('../models/db')
 
 /* GET users listing. */
 router.get('/', (req, res, next) => {
