1cdb1525a940eb6a46afb165e9928ae4d37f49d8
t-vo90 <thuan@hotmail.de>
2021-06-09 14:22:12 +0200

    Add dataBase Connection to the Project

---
+++ src/database-connection.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const username = process.env.MONGODB_USERNAME
+const password = process.env.MONGODB_PASSWORD
+const dbName = process.env.MONGODB_DATABASE
+
+mongoose.set('debug', true)
+
+mongoose
+  .connect(`mongodb+srv://${username}:${password}@cluster0.zh8uj.mongodb.net/${dbName}?retryWrites=true&w=majority`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connection established'))
+  .catch(console.log)
+
+// Create a New "Classes"/Model on the Database
+// const Panda = mongoose.model('Panda', { name: String, age: Number })
+
+// Async Function which awaits the Classes/Models you are specifying
+// async function main() {
+//   const pandas = await Panda.find({ age: { $lte: 21 } })
+//   console.log(pandas)
+// }
+
+// main()
+
+// const panda = new Panda({ name: 'Robert', age: 18 })
+//
+// panda.save().then(() => console.log(`we have a new panda ${panda.name}`))
