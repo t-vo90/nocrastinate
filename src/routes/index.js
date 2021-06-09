11c87edc9aa38596c08f0b37970c6c9dc3661109
t-vo90 <thuan@hotmail.de>
2021-06-09 14:27:15 +0200

    Correct require variables

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,11 @@ const express = require('express')
 
 const router = express.Router()
 
-const User = require('../models/db')
+const user = require('../models/db')
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'NoCrastinate' })
+  res.render('index', { title: 'NoCrastinate', user })
 })
 
 module.exports = router
