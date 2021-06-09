cca4d01b58a63af7976410f70ed3dacd2736aa99
t-vo90 <thuan@hotmail.de>
2021-06-09 14:27:45 +0200

    Add comment lines with database tasks

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,23 +1,37 @@
 /* eslint-disable no-unused-vars */
+const { query } = require('express')
 const express = require('express')
-const { request } = require('../app')
 
 const router = express.Router()
-
-const User = require('../models/db')
+const users = require('../models/db')
 
 /* GET users listing. */
 router.get('/', (req, res, next) => {
+  // router.get('/', async (req, res, next) => {
+  // const users = await User.find()
   let result = users
-
+  // let query = {}
   if (req.query.name) {
+    // query.name = req.query.name
     result = users.filter(user => user.name.toLowerCase() === req.query.name.toLowerCase())
   }
 
-  res.send(result)
-})
+  if (req.query.age) {
+    // query.age = req.query.age
+    result = users.filter(user => user.age === req.query.age)
+  }
 
+  res.send(result)
+  // res.send(await User.find())
+})
+// router.get('/initialize', async (req, res) => {
+//   await initalizeProject()
+//   console.log()
+//   res.sendStatus(200)
+// })
 router.get('/:userId', (req, res, next) => {
+  // router.get('/:userId', async (req, res, next) => {
+  // const user = await User.findById(req.params.userID)
   const user = users[req.params.userId]
   if (user) res.send(user)
   else res.sendStatus(404)
