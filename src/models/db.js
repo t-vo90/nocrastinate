16b4aa7a1115aae62412a271d28e6ad9c45da9dc
t-vo90 <thuan@hotmail.de>
2021-06-09 14:26:17 +0200

    Modify db.js file to do database tasks

--- src/models/db.js
+++ src/models/db.js
@@ -1,54 +1,33 @@
-/* CRUD Operations (CREATE UPDATE DELETE)
-ENDPOINT is adress to resource
-
-Routes:
-
-/user -> gets all users
-/user/:id -> gets one user
-POST /users -> create a user
-Delete /users/:id -> deletes the user
-Patch /user/:id -> update a user
-
-/offers -> gets all offers
-/offer/:id -> gets one offer
-
-Get -> gets the current state of resource
-Post -> create a new resource
-Delete -> delete a resource
-
-Put -> updates an entire resource
-Patch -> updates part of resource
-
-Requests have:
-* type of request
-* target endpoint
-* body
-* headers
-...
-
-Response have:
-*headers
-*body
-*status code
-...
-*/
 const User = require('./user')
 
-const thuan = new User('Thuan', 31, 'Mechanical Engineer', 'Germany')
-thuan.bio = ' On a Mission '
-const ozan = new User('Ozan', 24, 'Electrical Engineer', 'Turkey')
-ozan.bio = ' I am on my Way '
+const user = []
 
-const codingAction = thuan.createAction('Coding')
-const gymAction = thuan.createAction('Exercising')
+async function initalizeProject() {
+  const thuan = await User.create({ name: 'Thuan', age: 31, occupation: 'Mechanical Engineer', location: 'Germany' })
+  const ozan = await User.create({ name: 'Ozan', age: 24, occupation: 'Electrical Engineer', location: 'Turkey' })
+  // const thuan = new User('Thuan', 31, 'Mechanical Engineer', 'Germany')
+  // thuan.bio = ' On a Mission '
+  // const ozan = new User('Ozan', 24, 'Electrical Engineer', 'Turkey')
+  // ozan.bio = ' I am on my Way '
+  //
+  const codingAction = thuan.createAction({ Action: 'Coding' })
+  const gymAction = thuan.createAction({ Action: 'Exercising' })
 
-thuan.startAction(codingAction)
-thuan.stopAction(5)
+  thuan.startAction(codingAction)
+  thuan.stopAction(5)
 
-thuan.startAction(gymAction)
-thuan.stopAction(1)
+  thuan.startAction(gymAction)
+  thuan.stopAction(1)
 
-thuan.startAction(gymAction)
-thuan.stopAction(1)
+  thuan.startAction(gymAction)
+  thuan.stopAction(1)
 
-const users = [thuan, ozan]
+  user.push([thuan, ozan])
+  ;[thuan, ozan].forEach(element => element.save())
+
+  console.log(user)
+}
+
+initalizeProject()
+
+module.exports = user
