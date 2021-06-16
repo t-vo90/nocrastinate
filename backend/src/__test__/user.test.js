2f336f66b6d49465a54073ba984f6b63a0164dee
t-vo90 <thuan@hotmail.de>
2021-06-16 20:04:17 +0200

    Add tests and Vue

---
+++ backend/src/__test__/user.test.js
@@ -0,0 +1,11 @@
+const User = require('../models/user')
+const Action = require('../models/action')
+const ActionRecord = require('../models/actionRecord')
+
+describe('user library', () => {
+  test('user should do something', () => {
+    const expectedOutput = 0
+    const actualOutput = 0
+    expect(actualOutput).toBe(expectedOutput)
+  })
+})
--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -21,6 +21,7 @@ router.get('/', async (req, res, next) => {
 
   res.send(await User.find(query))
 })
+
 router.get('/initialize', async (req, res) => {
   const thuan = await User.create({ name: 'Thuan', age: 31, occupation: 'Mechanical Engineer', location: 'Germany' })
   const ozan = await User.create({ name: 'Ozan', age: 24, occupation: 'Electrical Engineer', location: 'Turkey' })
@@ -31,10 +32,17 @@ router.get('/initialize', async (req, res) => {
   await thuan.startAction(codingAction)
   // thuan.stopAction(5)
 
-  await thuan.startAction(gymAction)
+  // await thuan.startAction(gymAction)
 
   console.log(thuan)
   res.sendStatus(200)
+})
+
+router.get('/:name', async (req, res) => {
+  const user = await User.find({ name: req.params.name })
+  console.log('RIGHT USER', user)
+  if (user[0]) res.render('user', { user: user[0] })
+  else res.sendStatus(404)
 })
 
 router.get('/:userId', async (req, res) => {
@@ -42,6 +50,27 @@ router.get('/:userId', async (req, res) => {
   console.log(user)
   if (user) res.render('user', { user })
   else res.sendStatus(404)
+})
+
+router.get('/resetdb', async (req, res) => {
+  await User.deleteMany({})
+  await Action.deleteMany({})
+  await ActionRecord.deleteMany({})
+
+  res.sendStatus(200)
+})
+
+router.post('/', async (req, res) => {
+  const createdUser = await User.create(req.body)
+  res.send(createdUser)
+})
+
+router.post('/:userId/creates', async (req, res) => {
+  const user = await User.findById(req.params.use + -rId)
+  const action = await Action.findById(req.params.photoId)
+
+  await user.createAction(action)
+  res.sendStatus(200)
 })
 
 module.exports = router
