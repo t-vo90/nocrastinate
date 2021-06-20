9f27ad412d042c8d359225b438bcb7194c5e9bea
t-vo90 <thuan@hotmail.de>
2021-06-20 10:37:51 +0200

    Configure files to work with vue.js

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,14 +1,16 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
-  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
+  extends: ['eslint:recommended', 'plugin:vue/recommended', 'prettier/vue', 'plugin:prettier/recommended'],
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: 'babel-eslint'
   },
+  plugins: ['prettier'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  },
+    'vue/component-name-in-template-casing': ['error']
+  }
 }
