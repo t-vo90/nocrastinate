9d6a018973784593061c53bcebb3421f4f01a8f2
t-vo90 <thuan@hotmail.de>
2021-06-21 23:33:04 +0200

    Frontend: Remove unneeded Links

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
   #app
     #nav
-      router-link(to="/") Home  |&nbsp;
-      router-link(to="/about") About
+      router-link(to="/")
+      router-link(to="/about")
     router-view
 </template>
 
