4a0f055803e6f2bf807380dcb4b7dcdd17a83de8
t-vo90 <thuan@hotmail.de>
2021-06-29 23:54:00 +0200

    Modify Store to safe States

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,11 +1,64 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+const mutations = {
+  INCREMENT_COUNT: 'incrementCount',
+  SET_USER: 'set user'
+}
+
+const store = new Vuex.Store({
+  state: {
+    count: 0,
+    user: null
+  },
+  mutations: {
+    [mutations.INCREMENT_COUNT](state) {
+      state.count++
+    },
+    [mutations.SET_USER](state, user) {
+      state.user = user
+    }
+  },
+  actions: {
+    incrementCount({ commit }) {
+      commit(mutations.INCREMENT_COUNT)
+    },
+    async fetchUser(store, id) {
+      const userRequest = await axios.get(`/api/users/${id}`)
+      return userRequest.data
+    },
+    async fetchUsers() {
+      const usersRequest = await axios.get('/api/users')
+      return usersRequest.data
+    },
+    async fetchSession({ commit }) {
+      const user = await axios.get('/api/account/session')
+      commit(mutations.SET_USER, user.data || null)
+    },
+    async login({ commit }, credentials) {
+      try {
+        const user = await axios.post('/api/account/session', credentials)
+        commit(mutations.SET_USER, user.data)
+      } catch (e) {
+        throw e
+      }
+    },
+    async register(store, user) {
+      return axios.post('/api/account', user)
+    },
+    async logout({ commit }) {
+      await axios.delete('/api/account/session')
+      commit(mutations.SET_USER, null)
+    }
+  },
+
   modules: {}
 })
+
+export default async function init() {
+  await store.dispatch('fetchSession')
+  return store
+}
