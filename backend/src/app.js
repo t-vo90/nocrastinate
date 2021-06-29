902399e4cb035a3dd8a14e258cb2994f6cb9c0cd
t-vo90 <thuan@hotmail.de>
2021-06-29 23:40:07 +0200

    Add Session and Cookies to Backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,15 +3,21 @@
 /* eslint-disable prefer-arrow-callback */
 /* eslint-disable no-var */
 const createError = require('http-errors')
-const path = require('path')
 const express = require('express')
+const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
+const passport = require('passport')
+const User = require('./models/user')
+const AnimeQuote = require('./lib/quotes')
 
-require('./database-connection')
+const mongooseConnection = require('./database-connection')
 
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
+const accountRouter = require('./routes/account')
 
 const app = express()
 
@@ -32,9 +38,43 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+
+app.use(
+  session({
+    secret: ['thisisnotasupersecuresecretsecret', 'thisisanothersupernotsosecretsecret'],
+    saveUninitialized: false,
+    resave: false,
+    store: MongoStore.create({
+      mongoUrl: mongooseConnection._connectionString,
+      // mongoUrl: 'mongodb://mongo/nocrastinate',
+      // mongoUrl: process.env.MONGODB_CONNECTION_STRING,
+      stringify: false,
+    }),
+    cookie: {
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      path: '/api',
+    },
+  })
+)
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.use(User.createStrategy())
+
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use('/api', (req, res, next) => {
+  req.session.viewCount = req.session.viewCount || 0
+  req.session.viewCount++
+  next()
+})
+
 app.use('/api/', indexRouter)
+app.use('/api/account', accountRouter)
 app.use('/api/users', usersRouter)
 
 // catch 404 and forward to error handler
@@ -51,9 +91,16 @@ app.use((err, req, res) => {
 
   // render the error page
   res.status(err.status || 500)
-  res.render('error')
+
+  res.send({
+    status: err.status,
+    message: err.message,
+    stack: req.app.get('env') == 'development' ? err.stack : '',
+  })
 })
 
+const quote = AnimeQuote()
+console.log(quote)
 console.log('Creator what is my Purpose')
 
 module.exports = app
