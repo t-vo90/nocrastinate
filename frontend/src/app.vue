c976ce5d7b0dc8c050acc483a3f59b5162b8720b
t-vo90 <thuan@hotmail.de>
2021-06-29 23:49:37 +0200

    Add router links for Authentication

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -1,8 +1,25 @@
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  name: 'App',
+  methods: {
+    ...mapActions(['logout']),
+    async doLogout() {
+      await this.logout()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
 <template lang="pug">
   #app
     #nav
-      router-link(to="/")
-      router-link(to="/about")
+      router-link(to="/") Home
+      router-link(to="/profile") Profile
+      router-link(to="/login") Login
+      router-link(to="/register") Register
+      a(@click="doLogout" href="#") Logout
     router-view
 </template>
 
@@ -20,6 +37,7 @@
   a {
     font-weight: bold;
     color: #2c3e50;
+    margin: 0 1rem;
 
     &.router-link-exact-active {
       color: #42b983;
