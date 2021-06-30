2b5bdceb33f7558edc1ba91b772782f2b87b4ab3
t-vo90 <thuan@hotmail.de>
2021-06-30 19:36:41 +0200

    Create Socket-service.js File

---
+++ backend/src/socket-service.js
@@ -0,0 +1,33 @@
+const io = require('socket.io')()
+
+io.on('connect', socket => {
+  socket.emit('Connection Established!')
+
+  //   setInterval(() => {
+  //     socket.emit('hello world!')
+  //   }, 2000)
+
+  //   socket.on('new message', cb => {
+  //     console.log(' a new dawn')
+  //     cb('this is a response!')
+  //   })
+
+  socket.on('new message', (streamId, message) => {
+    socket.to(streamId).emit('new live stream message', message)
+  })
+
+  socket.on('join stream', streamId => {
+    socket.join(streamId)
+  })
+
+  socket.on('go live', (userId, cb) => {
+    console.log(`${userId} is going live`)
+
+    socket.broadcast.emit('new live stream', userId)
+    socket.join(userId)
+
+    cb(true)
+  })
+})
+
+module.exports = io
