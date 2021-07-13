567f22ddf684bf7fcb3a8617ff9a97b0ac017985
t-vo90 <thuan@hotmail.de>
2021-07-13 14:32:55 +0200

    Remove unneeded console.log

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,8 +4,6 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
 
-console.log('----------->')
-
 /* GET home page. */
 router.get('/', async (req, res, next) => {
   const users = await User.find({})
