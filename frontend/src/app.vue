660ee134b30d7b73335c3c27c2b9fb325d06ce9b
t-vo90 <thuan@hotmail.de>
2021-07-13 14:33:39 +0200

    Implement NavBar into Appdefault

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -1,4 +1,5 @@
 <script>
+import NavBar from '@/components/nav-bar.vue'
 import { mapActions } from 'vuex'
 
 export default {
@@ -9,21 +10,24 @@ export default {
       await this.logout()
       this.$router.push('/login')
     }
-  }
+  },
+  components: { NavBar }
 }
 </script>
 <template lang="pug">
   #app
-    #nav
-      router-link(to="/") Home
-      router-link(to="/profile") Profile
-      router-link(to="/login") Login
-      router-link(to="/register") Register
-      a(@click="doLogout" href="#") Logout
+    NavBar
+    //- #nav
+      //- router-link(to="/") Home
+      //- router-link(to="/profile") Profile
+      //- router-link(to="/login") Login
+      //- router-link(to="/register") Register
+      //- a(@click="doLogout" href="#") Logout
     router-view
 </template>
 
 <style lang="scss">
+@import url('https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css');
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
